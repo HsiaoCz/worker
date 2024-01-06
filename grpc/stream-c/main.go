77de3f84dd75d49de6565e9f09c9a725e038fc51
HsiaoCz <984274788@qq.com>
2024-01-06 22:21:30 +0800

    grpc

--- grpc/stream-c/main.go
+++ grpc/stream-c/main.go
@@ -1,5 +1,121 @@
 package main
 
-func main(){
-	
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"github.com/HsiaoCz/worker/grpc/stream-c/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// 一元grpc
+func Login(c pb.GreeterClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Login(ctx, &pb.LoginRequest{Username: "zhangsan", Password: "1233abc", Repassword: "1233abc"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(r.GetMsg())
+}
+
+// 服务端流式grpc
+func Hello(c pb.GreeterClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	stream, err := c.Hello(ctx, &pb.HelloRequest{Content: "hello,my name is zhangsan"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		res, err := stream.Recv()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(res.GetSomething())
+	}
+}
+
+// 客户端流式grpc
+func HandleHi(c pb.GreeterClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	stream, err := c.HandleHi(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := 0; i < 10; i++ {
+		if err = stream.Send(&pb.HIRequest{Conent: GenHI()}); err != nil {
+			log.Fatal(err)
+		}
+	}
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res.GetSomething())
+}
+
+// 双向流式grpc
+func HandleChat(c pb.GreeterClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	stream, err := c.HandleChat(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	watic := make(chan struct{})
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err != nil {
+				log.Fatal(err)
+			}
+			if err == io.EOF {
+				close(watic)
+				return
+			}
+			fmt.Println("Anwser:", res.GetAnwser())
+			fmt.Println("PID:", res.GetPid())
+		}
+	}()
+	names := []string{"zhasss", "bob", "andi", "hall", "will"}
+	for _, name := range names {
+		if err = stream.Send(&pb.ChatRequest{Name: name, Chatcontent: GenHI()}); err != nil {
+			log.Fatal(err)
+		}
+	}
+	stream.CloseSend()
+	<-watic
+}
+
+var (
+	addr = "127.0.0.1:9001"
+)
+
+func GenHI() string {
+	randm := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return strconv.Itoa(randm.Intn(1000000))
+}
+
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+	Login(c)
+	Hello(c)
+	HandleChat(c)
+	HandleHi(c)
 }
--- grpc/stream-s/main.go
+++ grpc/stream-s/main.go
@@ -2,26 +2,94 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net"
+	"strconv"
+	"time"
 
 	"github.com/HsiaoCz/worker/grpc/stream-s/pb"
+	"google.golang.org/grpc"
 )
 
-type StreamService struct{
+type StreamService struct {
 	pb.UnimplementedGreeterServer
 }
 
-func NewStreamService()*StreamService{
+func NewStreamService() *StreamService {
 	return &StreamService{}
 }
 
-func (s *StreamService)Login(ctx context.Context,in *pb.LoginRequest)(*pb.LoginResponse,error){
-	return &pb.LoginResponse{},nil
+func (s *StreamService) Login(ctx context.Context, in *pb.LoginRequest) (*pb.LoginResponse, error) {
+	if in.GetPassword() != in.GetRepassword() {
+		return &pb.LoginResponse{Msg: "请确认用户密码"}, nil
+	}
+	return &pb.LoginResponse{Msg: "登录成功"}, nil
 }
 
-func (s *StreamService)Hello(ctx context.Context,in *pb.HelloRequest)(*pb.HelloResponse,error){
-	return &pb.HelloResponse{},nil
+func (s *StreamService) Hello(in *pb.HelloRequest, server pb.Greeter_HelloServer) error {
+	fmt.Println(in.Content)
+	words := []string{"你好", "hello", "halo", "sawadika", "bonjor"}
+	for _, word := range words {
+		data := &pb.HelloResponse{Something: word}
+		if err := server.Send(data); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func main(){
-	
+func (s *StreamService) HandleHi(stream pb.Greeter_HandleHiServer) error {
+	for {
+		res, err := stream.Recv()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.HIResponse{
+				Something: "你好,my man",
+			})
+		}
+		fmt.Println(res.GetConent())
+	}
+}
+
+func (s *StreamService) HandleChat(stream pb.Greeter_HandleChatServer) error {
+	for {
+		res, err := stream.Recv()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err == io.EOF {
+			return stream.Send(&pb.ChatResponse{
+				Anwser: "你好:" + res.GetName(),
+				Pid:    GenPID(),
+			})
+		}
+		fmt.Println()
+	}
+}
+
+func GenPID() string {
+	randm := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return strconv.Itoa(randm.Intn(1000000))
+}
+
+var (
+	network = "tcp"
+	addr    = "127.0.0.1:9001"
+)
+
+func main() {
+	lis, err := net.Listen(network, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, NewStreamService())
+	if err := s.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
 }
