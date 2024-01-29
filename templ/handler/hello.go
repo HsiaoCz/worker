46a3568d9d1dcb3222fff8251c417036e06c48fd
HsiaoCz <984274788@qq.com>
2024-01-29 20:25:12 +0800

    templ

--- templ/handler/hello.go
+++ templ/handler/hello.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"context"
+	"io"
+	"log/slog"
 	"net/http"
 
 	"github.com/HsiaoCz/worker/templ/hello"
@@ -11,4 +13,15 @@ func HandleHello(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
 	comp := hello.Hello(name)
 	comp.Render(context.Background(), w)
+}
+
+func HandleButton(w http.ResponseWriter, r *http.Request) {
+	text, err := io.ReadAll(r.Body)
+	if err != nil {
+		slog.Error("read request body err:", err)
+		return
+	}
+
+	button := hello.Button(string(text))
+	button.Render(context.Background(), w)
 }
--- templ/hello/hello_templ.go
+++ templ/hello/hello_templ.go
@@ -46,3 +46,40 @@ func Hello(name string) templ.Component {
 		return templ_7745c5c3_Err
 	})
 }
+
+func Button(text string) templ.Component {
+	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
+		if !templ_7745c5c3_IsBuffer {
+			templ_7745c5c3_Buffer = templ.GetBuffer()
+			defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var3 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var3 == nil {
+			templ_7745c5c3_Var3 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<button class=\"button\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var4 string
+		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(text)
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `hello.templ`, Line: 7, Col: 30}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</button>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		if !templ_7745c5c3_IsBuffer {
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteTo(templ_7745c5c3_W)
+		}
+		return templ_7745c5c3_Err
+	})
+}
