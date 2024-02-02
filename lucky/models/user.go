64e40c546c6f92535d43183c73a50881daaa1845
HsiaoCz <984274788@qq.com>
2024-02-03 00:08:34 +0800

    fiber lucky

--- lucky/models/user.go
+++ lucky/models/user.go
@@ -1,10 +1,18 @@
 package models
 
 type User struct {
+	Username string `json:"username"`
+	Today    string `json:"today"`
+	Luck     Lucky  `json:"luck"`
 }
 
 type Lucky struct {
+	Value   int    `json:"value"`
+	Level   string `json:"level"`
+	Explain string `json:"explain"`
 }
 
 type Post struct {
+	Username    string `json:"username"`
+	RequestText string `json:"request_text"`
 }
