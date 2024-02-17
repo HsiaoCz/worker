0e47b5f9b0d6a9d64cb7952674db0d3b54fe4b04
HsiaoCz <984274788@qq.com>
2024-02-17 19:48:50 +0800

    leo

---
+++ leo/handlers/user.go
@@ -0,0 +1,11 @@
+package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+func HandleUserSignup(c *fiber.Ctx) error {
+	return nil
+}
+
+func HandleUserLogin(c *fiber.Ctx) error {
+	return nil
+}
---
+++ leo/post/user.go
@@ -0,0 +1,5 @@
+package post
+
+type UserSignup struct{}
+
+type UserLogin struct{}
