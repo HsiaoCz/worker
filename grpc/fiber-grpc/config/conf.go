6c0079c8282e950fcee3f3adaac033c9fbbad59e
HsiaoCz <984274788@qq.com>
2024-02-22 21:24:49 +0800

    fiber grpc

--- grpc/fiber-grpc/config/conf.go
+++ grpc/fiber-grpc/config/conf.go
@@ -1,7 +1,59 @@
 package config
 
+import (
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+var Conf = new(FiberGrpc)
+
 type FiberGrpc struct {
-	App AppConf
+	App   AppConf   `mapstructure:"app"`
+	Mysql MysqlConf `mapstructure:"mysql"`
+	Log   LogConf   `mapstructure:"log"`
 }
 
-type AppConf struct{}
+type AppConf struct {
+	Addr      string `mapstructure:"addr"`
+	StartTime string `mapstructure:"start_time"`
+	MachineID int    `mapstructure:"machine_id"`
+}
+type MysqlConf struct {
+	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	DBname   string `mapstructure:"dbname"`
+}
+type LogConf struct {
+	Level      string `mapstructure:"level"`
+	Filename   string `mapstructure:"filename"`
+	MaxSize    int    `mapstructure:"max_size"`
+	MaxAge     int    `mapstructure:"max_age"`
+	MaxBackups int    `mapstructure:"max_backups"`
+}
+
+func ParseConf() (err error) {
+	viper.SetConfigName("fiber-grpc")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./")
+	err = viper.ReadInConfig()
+	if err != nil {
+		zap.L().Error("viper read config err:", zap.Error(err))
+		return
+	}
+	if err := viper.Unmarshal(Conf); err != nil {
+		zap.L().Error("viper unmarshal config err:", zap.Error(err))
+		return err
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		zap.L().Info("config changed....")
+		if err := viper.Unmarshal(Conf); err != nil {
+			zap.L().Error("reunmarshal config err:", zap.Error(err))
+			return
+		}
+	})
+	return err
+}
--- grpc/fiber-grpc/internal/data/user.go
+++ grpc/fiber-grpc/internal/data/user.go
@@ -1,15 +1,17 @@
 package data
 
 type User struct {
-	Username string
-	Password string
-	Identity string
+	Username string `gorm:"column:username;type:varchar(100);" json:"username"`
+	Password string `gorm:"column:password;type:varchar(100);" json:"password"`
+	Identity string `gorm:"column:identity;type:varchar(100);" json:"identity"`
 	// 简介
-	Content string
+	Content string `gorm:"column:content;type:varchar(300);" json:"content"`
 	// 等级
-	Level int
+	Level int `gorm:"column:level;type:int(3);" json:"level"`
 	// 头像
-	Vargar string
+	Vargar string `gorm:"column:vargar;type:varchar(200);" json:"vargar"`
+	// email
+	Email string `gorm:"column:email;type:varchar(100);" json:"email"`
 }
 
 func (u User) TableName() string {
