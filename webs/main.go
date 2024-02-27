3fd518d1a1e778e22e8f5d108311cab495656c67
HsiaoCz <984274788@qq.com>
2024-02-27 17:45:50 +0800

    websocket chat-gpt generate code

---
+++ webs/main.go
@@ -0,0 +1,47 @@
+package main
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
+	for {
+		messageType,p,err:=ws.ReadMessage()
+		if err!=nil{
+			log.Println(err)
+			break
+		}
+		log.Println(string(p))
+		err=ws.WriteMessage(messageType,p)
+		if err!=nil{
+			log.Println(err)
+			break
+		}
+	}
+}
+
+func main(){
+// Create a simple file server
+	fs := http.FileServer(http.Dir("../public"))
+	http.Handle("/", fs)
+
+	// Configure websocket route
+	http.HandleFunc("/ws", handleConnections)
+
+	// Start the server on localhost port 8000 and log any errors
+	println("http server started on :8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		panic("Error: " + err.Error())
+	}
+}
